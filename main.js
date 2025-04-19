afdbc8a8e56f3da0bf69b5643074753e6e42877f
anuj-weebjs <weeb.js@yahoo.com>
2025-04-19 16:07:55 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+const { Client, WebhookClient } = require('discord.js-selfbot-v13');
+const client = new Client();
+const token = process.env.TOKEN_DETOXI;
+const makimaUserId = "1242713384810582066";
+const makimaChannelId = "1363083325723377714";
+const aoiUserId = "1274410272513200128";
+const aoiChannelId = "1362743916641910855";
+const aoiWebhookUrl = "https://discord.com/api/webhooks/1363079989968240640/NKEBHpwkeTSgD5YvO-NZcijMJbW24ShaM7Smi2GRwoG3_hrCg0wY4pgKCRXH-_Ns8SxX"
+
+
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+})
+
+client.on('messageCreate', async(message) => {
+  if(!message.content){
+    return;
+  }
+
+  console.log(`${message.guild.name}/${message.author.username}:${message.content}`)
+  
+  if(message.author.id == aoiUserId){  
+    
+    if(message.channel.id == aoiChannelId){
+      await client.channels.cache.get(makimaChannelId).send(message.content);
+    }
+
+    return;
+  }
+
+  if(message.channel.id == makimaChannelId && message.author.id != "1172174732913823858") {
+    const webhookClient = new WebhookClient({ url: aoiWebhookUrl });
+
+    let name;
+    if(!message.author.globalName){
+      name = message.author.username;
+    }else{
+      name = message.author.globalName
+    }
+    
+    webhookClient.send({
+      content: message.content,
+      username: name,
+      avatarURL: message.author.avatarURL()
+    });
+  }
+})
+
+client.login(token);
